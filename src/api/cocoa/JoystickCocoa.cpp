14c13383d3f196f6422d06550665213982e66088
Alwin Esch <alwin.esch@web.de>
2017-02-17 22:07:08 +0100

    Update fix

--- src/api/cocoa/JoystickCocoa.cpp
+++ src/api/cocoa/JoystickCocoa.cpp
@@ -143,7 +143,7 @@ void CJoystickCocoa::Deinitialize(void)
   m_bInitialized = false;
 }
 
-bool CJoystickCocoa::GetEvents(std::vector<kodi::kodi::addon::PeripheralEvent>& events)
+bool CJoystickCocoa::GetEvents(std::vector<kodi::addon::PeripheralEvent>& events)
 {
   CLockObject lock(m_mutex);
   return CJoystick::GetEvents(events);
